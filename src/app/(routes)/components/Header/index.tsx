17b4bc972150ec6fbc52bec4fab6b8cfcf2a3ab6
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-04 14:18:00 +0700

    feat(header): add layout for header component

--- src/app/(routes)/components/Header/index.tsx
+++ src/app/(routes)/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import Logo from "@/components/Logo";
+
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header className="bg-white border-b">
+      <div className="container mx-auto flex justify-between">
+        <div>
+          <Logo />
+        </div>
+        <div className="flex">
+          <div>Search</div>
+          <div>User</div>
+        </div>
+      </div>
+    </header>
+  );
 }
