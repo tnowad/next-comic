c603287b8a19c6ab0bb9271a4e5cb0d051323a16
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-04 13:56:33 +0700

    feat(component): add layout component for home

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div></div>;
+  return <main>Home</main>;
 }
---
+++ src/app/(routes)/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
