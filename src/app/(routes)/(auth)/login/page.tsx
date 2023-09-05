af6dbea83aa516a803a8e54f39da81c26bdd48d5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-05 14:10:14 +0700

    feat(component): add layout for auth route

--- src/app/(routes)/(auth)/login/page.tsx
+++ src/app/(routes)/(auth)/login/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+import Login from "../components/Login";
+
+export const metadata: Metadata = {
+  title: "Login | NextComic",
+};
+
 export default function Page() {
-  return <div></div>;
+  return <Login />;
 }
