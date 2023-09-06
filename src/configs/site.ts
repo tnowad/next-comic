c979558ad7895b4cece5998cd005b24fc27fc3fa
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-06 16:53:12 +0700

    feat(config): add site config

---
+++ src/configs/site.ts
@@ -0,0 +1,41 @@
+const siteConfig = {
+  name: "NextComic",
+  description: "",
+  ogImage: "",
+  author: "Nguyen Minh Tuan",
+  email: "tnowad@gmail.com",
+  siteUrl: "",
+  creator: "tnowad",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "",
+    siteName: "NextComic",
+    description: "",
+    images: [
+      {
+        url: "",
+        width: 1200,
+        height: 630,
+        alt: "NextComic",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NextComic",
+    description: "",
+    image: "",
+    creator: "tnowad",
+  },
+  links: {
+    github: "https://github.com/tnowad/next-comic",
+    twitter: "",
+    docs: "",
+    discord: "",
+    sponsor: "",
+    portfolio: "https://portfolio.tnowad.vercel.app",
+  },
+};
+
+export default siteConfig;
