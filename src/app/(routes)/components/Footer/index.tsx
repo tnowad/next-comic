39161416a40bcd08a90ed70ebfd733039924c0b8
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-06 16:54:02 +0700

    feat(component): add footer component

--- src/app/(routes)/components/Footer/index.tsx
+++ src/app/(routes)/components/Footer/index.tsx
@@ -1,12 +1,31 @@
-import Logo from "@/components/Logo";
+"use client";
+
+import dynamic from "next/dynamic";
+import { Link } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+
+import siteConfig from "@/configs/site";
 
 export default function Footer() {
+  const pathname = usePathname();
+
+  if (pathname.includes("/examples")) {
+    return null;
+  }
+
   return (
-    <footer className="border-t mt-auto">
-      <div className="container mx-auto">
-        <div>
-          <Logo />
-        </div>
+    <footer className="container mx-auto max-w-7xl pb-12 px-12">
+      <div className="flex flex-col justify-center items-center gap-1">
+        <p className="text-sm text-default-400">
+          Created by{" "}
+          <Link
+            isExternal
+            className="text-sm"
+            href={siteConfig.links.portfolio}
+          >
+            {siteConfig.author}
+          </Link>
+        </p>
       </div>
     </footer>
   );
