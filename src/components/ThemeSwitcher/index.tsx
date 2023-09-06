e889088bb2817087db4cfed1b99e1a95c2955d50
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-06 07:00:27 +0700

    refactor(theme-switcher): refactor onChange function

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import { Switch } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
@@ -6,9 +7,9 @@ import { useTheme } from "next-themes";
 
 export default function ThemeSwitcher() {
   const { setTheme, theme } = useTheme();
-  const handleToggleTheme = () => {
-    const nextTheme = theme === "dark" ? "light" : "dark";
-    setTheme(nextTheme);
+
+  const onChange = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
   };
 
   return (
@@ -16,7 +17,7 @@ export default function ThemeSwitcher() {
       defaultSelected
       size="lg"
       defaultChecked={theme == "dark"}
-      onChange={handleToggleTheme}
+      onChange={onChange}
       color="primary"
       startContent={<Icon icon="line-md:sun-rising-loop" />}
       endContent={<Icon icon="line-md:moon" />}
