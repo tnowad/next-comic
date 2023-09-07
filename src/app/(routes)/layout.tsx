7db649d51c56454d60894a1d30968b87b251965b
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-07 13:16:04 +0700

    fix(component): add missing attribute

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <main className="relative flex flex-col">
-        <Navbar />
+        <Navbar routes={[]} mobileRoutes={[]} />
         {children}
         <Footer />
       </main>
