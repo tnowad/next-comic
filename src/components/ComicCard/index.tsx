1a07c3724f178d8f41b5b9fb2f3ae111f649a430
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-09 20:59:55 +0700

    feat(component): add truncated title

--- src/components/ComicCard/index.tsx
+++ src/components/ComicCard/index.tsx
@@ -10,17 +10,26 @@ interface Chapter {
 interface ComicCardProps {
   comic: Comic;
   variant?: "default" | "";
+  maxTitleLength?: number;
 }
 
-export default function ComicCard({ comic }: ComicCardProps) {
+export default function ComicCard({
+  comic,
+  maxTitleLength = 50,
+}: ComicCardProps) {
+  const truncatedTitle =
+    comic.title.length > maxTitleLength
+      ? comic.title.substring(0, maxTitleLength) + "..."
+      : comic.title;
+
   return (
     <Card radius="sm" className="grid grid-cols-[30%,_70%] sx:flex">
       <CardHeader className="overflow-visible sx:justify-center p-0">
         <Image className="w-fit" alt={comic.title} src={comic.coverImage} />
       </CardHeader>
-      <CardBody className="sx:justify-center p-1 w-full">
+      <CardBody className=" p-1 w-full">
         <NextLink href={`/comics/${comic.title}/${comic.id}`}>
-          <b className="text-center">{comic.title}</b>
+          <p className="capitalize whitespace-break-spaces">{truncatedTitle}</p>
         </NextLink>
         {comic.chapters.slice(-3).map((chapter) => (
           <Link
