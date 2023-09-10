5659f52450ed5a0ee8499511067be332461b8c86
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-10 15:54:21 +0700

    feat(component): add comic page

--- src/app/(routes)/(comic)/comics/[slug]/[comicId]/page.tsx
+++ src/app/(routes)/(comic)/comics/[slug]/[comicId]/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <div></div>;
+import ComicDetail from "@/components/ComicDetail";
+import FollowingComic from "@/components/FollowingComic";
+import LeaderBoardComic from "@/components/LeaderBoardComic";
+import ReadingHistoryComic from "@/components/ReadingHistoryComic";
+import { createRandomComic } from "@/mocks/comics";
+
+async function getComic() {
+  return createRandomComic();
+}
+
+export default async function Page() {
+  const comic = await getComic();
+
+  return (
+    <section className="container mx-auto">
+      <div className="xl:grid grid-cols-12">
+        <div className="col-span-8">
+          <ComicDetail comic={comic} />
+        </div>
+        <div className="col-span-4">
+          <FollowingComic />
+          <ReadingHistoryComic />
+          <LeaderBoardComic />
+        </div>
+      </div>
+    </section>
+  );
 }
