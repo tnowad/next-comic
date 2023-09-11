16f8f6b04d1159e6eb99424429ef2164f698bfa0
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-11 09:07:25 +0700

    feat(component): add skeleton for ComicList

--- src/components/ComicList/index.tsx
+++ src/components/ComicList/index.tsx
@@ -1,19 +1,19 @@
+import { Comic } from "@/types/comic";
 import ComicCard from "../ComicCard";
-import { createRandomComic } from "@/mocks/comics";
+import ComicCardSkeleton from "../ComicCard/ComicCardSkeleton";
+import { faker } from "@faker-js/faker";
 
-interface ComicListProps { }
-
-async function getAllComic() {
-  return Array.from({ length: 4 * 3 * 2 * 2 }).map(createRandomComic);
+interface ComicListProps {
+  comics?: Comic[]
 }
-
-export default async function ComicList({ }: ComicListProps) {
-  const comics = await getAllComic();
-
+export default function ComicList({ comics }: ComicListProps) {
+  const isLoaded = !!comics
   return (
     <div className="grid grid-cols-1 gap-3 sx:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-      {comics.map((comic) => (
+      {isLoaded ? comics.map((comic) => (
         <ComicCard comic={comic} key={comic.id} />
+      )) : Array.from({ length: 24 }).map(() => (
+        <ComicCardSkeleton key={faker.string.uuid()} />
       ))}
     </div>
   );
