5d081e10bd26a9aeabef6427dffcecf8d4fedb6c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-11 07:00:29 +0700

    feat(component): add page ReadComic

--- src/app/(routes)/(comic)/chapter/[slug]/[chapterId]/page.tsx
+++ src/app/(routes)/(comic)/chapter/[slug]/[chapterId]/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
-  return <div className="flex w-full flex-col"></div>;
+import ChapterRead from "@/components/ChapterRead";
+import { createRandomChapter, createRandomComic } from "@/mocks/comics";
+import { ComicPreview } from "@/types/comic";
+import { faker } from "@faker-js/faker";
+
+async function getComicPreview() {
+  return createRandomComic() as ComicPreview;
+}
+
+async function getChapter() {
+  return createRandomChapter();
+}
+
+async function getChapters() {
+  return faker.helpers.uniqueArray(createRandomChapter, 100);
+}
+
+export default async function Page() {
+  const comicPreview = await getComicPreview();
+  const chapter = await getChapter();
+  const chapters = await getChapters();
+
+  return (
+    <div className="flex w-full flex-col">
+      {comicPreview.title}
+      {chapters.map((chapter) => (
+        <div key={chapter.id}>{chapter.title}</div>
+      ))}
+      {chapter.title}
+      <ChapterRead chapter={chapter} />
+    </div>
+  );
 }
