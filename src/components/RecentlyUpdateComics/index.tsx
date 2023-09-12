b30bd72fdfefad82b446db9667435de1491a1e61
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-13 00:11:30 +0700

    feat(component): add RecentlyUpdateComics hero

--- src/components/RecentlyUpdateComics/index.tsx
+++ src/components/RecentlyUpdateComics/index.tsx
@@ -3,30 +3,41 @@
 import ComicList from "@/components/ComicList";
 import { createRandomComic } from "@/mocks/comics";
 import { Comic } from "@/types/comic";
-import { Pagination } from "@nextui-org/react";
+import { Button, Pagination } from "@nextui-org/react";
 import { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
+import { useRouter } from "next/navigation";
 
 async function getAllComic() {
   return Array.from({ length: 4 * 3 * 2 * 2 }).map(createRandomComic);
 }
 
-
 export default function RecentlyUpdateComics() {
   const [comics, setComics] = useState<Comic[]>();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchComics = async () => {
-      const comics = await getAllComic()
+      const comics = await getAllComic();
       setComics(comics);
-    }
-    fetchComics()
-  }, [])
+    };
+    fetchComics();
+  }, []);
 
   return (
     <div className="w-full">
-      <p>HeroSection</p>
+      <div className="mb-2 flex justify-between">
+        <p className="text-xl text-primary">Newly updated comics</p>
+        <Button
+          variant="ghost"
+          className="min-w-0 border-none text-xl outline-none"
+          onClick={() => router.push("/search")}
+        >
+          <Icon icon="solar:filter-outline" />
+        </Button>
+      </div>
       <ComicList comics={comics} />
-      <div className="flex justify-center">
+      <div className="my-2 flex justify-center">
         <Pagination total={10} initialPage={1} />
       </div>
     </div>
