a2c4e38ab7f7d418d45f7f794198b30e8ee5a36f
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-13 00:09:46 +0700

    refactor(component): format component

--- src/components/ComicList/index.tsx
+++ src/components/ComicList/index.tsx
@@ -1,20 +1,23 @@
+"use client";
+
 import { Comic } from "@/types/comic";
 import ComicCard from "../ComicCard";
 import ComicCardSkeleton from "../ComicCard/ComicCardSkeleton";
 import { faker } from "@faker-js/faker";
 
 interface ComicListProps {
-  comics?: Comic[]
+  comics?: Comic[];
 }
+
 export default function ComicList({ comics }: ComicListProps) {
-  const isLoaded = !!comics
+  const isLoaded = !!comics;
   return (
-    <div className="grid grid-cols-1 gap-3 sx:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-      {isLoaded ? comics.map((comic) => (
-        <ComicCard comic={comic} key={comic.id} />
-      )) : Array.from({ length: 24 }).map(() => (
-        <ComicCardSkeleton key={faker.string.uuid()} />
-      ))}
+    <div className="grid grid-cols-1 gap-3 sx:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-4">
+      {isLoaded
+        ? comics.map((comic) => <ComicCard comic={comic} key={comic.id} />)
+        : Array.from({ length: 24 }).map(() => (
+          <ComicCardSkeleton key={faker.string.uuid()} />
+        ))}
     </div>
   );
 }
