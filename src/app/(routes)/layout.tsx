9902f5471754bf79ad789ca929b41d184b9ea1f9
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-13 16:16:04 +0700

    refactor(component): move navbar and footer to components folder

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 interface LayoutProps {
   children: React.ReactNode;
