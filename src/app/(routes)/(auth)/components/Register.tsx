8a3c86370b295a768e5ab758ffbbbe55bb3ed242
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-13 20:36:21 +0700

    feat(component): add register component

--- src/app/(routes)/(auth)/components/Register.tsx
+++ src/app/(routes)/(auth)/components/Register.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { Button, Input, Link } from "@nextui-org/react";
+import NextLink from "next/link";
+
 export default function Register() {
-  return <div></div>;
+  return (
+    <form className="flex h-[300px] flex-col gap-4">
+      <Input
+        isRequired
+        label="Name"
+        placeholder="Enter your name"
+        type="password"
+      />
+      <Input
+        isRequired
+        label="Email"
+        placeholder="Enter your email"
+        type="email"
+      />
+      <Input
+        isRequired
+        label="Password"
+        placeholder="Enter your password"
+        type="password"
+      />
+      <p className="text-center text-small">
+        Already have an account?{" "}
+        <Link size="sm" as={NextLink} href="/login">
+          Login
+        </Link>
+      </p>
+      <div className="flex justify-end gap-2">
+        <Button fullWidth color="primary">
+          Sign up
+        </Button>
+      </div>
+    </form>
+  );
 }
