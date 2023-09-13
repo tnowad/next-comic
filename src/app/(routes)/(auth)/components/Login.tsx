672703c8121131135e470325bc417d3f6ee261d5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-13 20:35:59 +0700

    feat(component): add Login component

--- src/app/(routes)/(auth)/components/Login.tsx
+++ src/app/(routes)/(auth)/components/Login.tsx
@@ -1,3 +1,34 @@
+"use client";
+
+import { Button, Input, Link } from "@nextui-org/react";
+import NextLink from "next/link";
+
 export default function Login() {
-  return <div></div>;
+  return (
+    <form className="flex flex-col gap-4">
+      <Input
+        isRequired
+        label="Email"
+        placeholder="Enter your email"
+        type="email"
+      />
+      <Input
+        isRequired
+        label="Password"
+        placeholder="Enter your password"
+        type="password"
+      />
+      <p className="text-center text-small">
+        Need to create an account?{" "}
+        <Link size="sm" as={NextLink} href="/register">
+          Register
+        </Link>
+      </p>
+      <div className="flex justify-end gap-2">
+        <Button fullWidth color="primary">
+          Login
+        </Button>
+      </div>
+    </form>
+  );
 }
