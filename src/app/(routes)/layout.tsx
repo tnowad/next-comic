912b839480f19693b2e8a5d0a9cb4d67e4bf8b6a
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-13 20:35:25 +0700

    feat(component): change min height to 100vh

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
-      <main className="container relative z-10 mx-auto min-h-[calc(100vh_-_64px_-_108px)] max-w-7xl flex-grow">
+      <main className="container relative z-10 mx-auto min-h-screen max-w-7xl flex-grow">
         <Navbar routes={[]} mobileRoutes={[]} />
         {children}
         <Footer />
