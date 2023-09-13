2aa732b93f6559fae828c9c2d524763d167c2605
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-13 20:36:34 +0700

    feat(component): add layout for auth

---
+++ src/app/(routes)/(auth)/layout.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
+import { usePathname, useRouter } from "next/navigation";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [selected, setSelected] = useState(pathname);
+
+  useEffect(() => {
+    setSelected(pathname);
+  }, [pathname]);
+
+  return (
+    <div className="flex h-full w-full flex-col items-center">
+      <Card className="w-[340px] max-w-full">
+        <CardBody className="overflow-hidden">
+          <Tabs
+            fullWidth
+            size="md"
+            aria-label="Tabs form"
+            selectedKey={selected}
+            onSelectionChange={(key) => {
+              setSelected(key as string);
+              router.push(key as string);
+            }}
+          >
+            <Tab key="/login" title="Login">
+              {children}
+            </Tab>
+            <Tab key="/register" title="Sign up">
+              {children}
+            </Tab>
+          </Tabs>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
