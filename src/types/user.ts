cfa6fc2404d5268ed752267784b4ee24b092dbed
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-15 00:28:03 +0700

    feat(type): add type for Permission

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,13 +1,34 @@
+export interface PermissionPreview {
+  id: string;
+  name: string;
+}
+
+export interface Permission extends PermissionPreview {
+  description: string;
+}
+
+export interface RolePreview {
+  id: string;
+  name: string;
+  permissions: PermissionPreview[];
+}
+
+export interface Role extends RolePreview {
+  description: string;
+  permissions: Permission[];
+}
+
 export interface UserPreview {
   id: string;
   name: string;
   avatarImage: string;
-  roles: string[];
+  roles: RolePreview[];
   username: string;
   email: string;
 }
 
 export interface User extends UserPreview {
+  roles: Role[];
   birthday: Date;
   bio: string;
 }
