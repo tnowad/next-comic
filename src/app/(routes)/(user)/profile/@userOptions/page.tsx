b3137022baf5d067c3d146591aa573c6e0a81ef9
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-14 23:59:27 +0700

    feat(component): add margin top and flex

--- src/app/(routes)/(user)/profile/@userOptions/page.tsx
+++ src/app/(routes)/(user)/profile/@userOptions/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@nextui-org/react";
 
 export default function Page() {
   return (
-    <div className="grid gap-1">
+    <div className="mt-1 flex gap-1 md:grid">
       <Button color="primary">Follow</Button>
       <Button>Message</Button>
       <Button>Report</Button>
