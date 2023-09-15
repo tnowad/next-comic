9d218d3f3cbd8d9b49d6fb8859d933a498e2cdc4
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-15 12:58:14 +0700

    feat(component): add responsive for options button

--- src/app/(routes)/(user)/profile/@userOptions/page.tsx
+++ src/app/(routes)/(user)/profile/@userOptions/page.tsx
@@ -1,13 +1,60 @@
 "use client";
 
-import { Button } from "@nextui-org/react";
+import { Icon } from "@iconify/react";
+import {
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
+import React from "react";
 
 export default function Page() {
   return (
-    <div className="mt-1 flex gap-1 md:grid">
-      <Button color="primary">Follow</Button>
-      <Button>Message</Button>
-      <Button>Report</Button>
-    </div>
+    <React.Fragment>
+      <div className="mt-1 hidden gap-1 md:grid">
+        <Button startContent={<Icon icon="mdi:heart" />} color="primary">
+          Follow
+        </Button>
+        <Button startContent={<Icon icon="ant-design:message-outlined" />}>
+          Message
+        </Button>
+        <Button startContent={<Icon icon="material-symbols:report" />}>
+          Report
+        </Button>
+      </div>
+      <div className="flex md:hidden">
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              isIconOnly
+              startContent={<Icon icon="material-symbols:report" />}
+            />
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownItem
+              className="flex sx:hidden"
+              startContent={<Icon icon="mdi:heart" />}
+            >
+              Follow
+            </DropdownItem>
+            <DropdownItem
+              startContent={<Icon icon="ant-design:message-outlined" />}
+            >
+              Message
+            </DropdownItem>
+            <DropdownItem
+              startContent={<Icon icon="material-symbols:report" />}
+            >
+              Report
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Button startContent={<Icon icon="mdi:heart" />} color="primary">
+          <div className="max-sx:hidden">Follow</div>
+        </Button>
+      </div>
+    </React.Fragment>
   );
 }
