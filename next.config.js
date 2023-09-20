05e18e1af9f2627428d211ef9e3c7534fc065983
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-21 01:53:03 +0700

    config(next): add cache-control header

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   images: {
     minimumCacheTTL: 60,
   },
+  async headers() {
+    return [
+      {
+        source: "/images/:slug*",
+        headers: [{ key: "cache-control", value: "max-age=2592000" }],
+      },
+      {
+        source: "/fonts/:slug*",
+        headers: [{ key: "cache-control", value: "max-age=2592000" }],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
