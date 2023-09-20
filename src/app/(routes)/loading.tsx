02964fc57545fa79cae8edd7ef3403e628934747
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-21 01:52:29 +0700

    feat(component): change from NextImage to UnoptimizedNextImage

--- src/app/(routes)/loading.tsx
+++ src/app/(routes)/loading.tsx
@@ -1,9 +1,10 @@
 import NextImage from "@/components/NextImage";
+import UnoptimizedNextImage from "@/components/UnoptimizedNextImage";
 
 export default function Default() {
   return (
     <div className="flex w-full justify-center">
-      <NextImage
+      <UnoptimizedNextImage
         src={"/images/loading.gif"}
         height={370}
         width={300}
